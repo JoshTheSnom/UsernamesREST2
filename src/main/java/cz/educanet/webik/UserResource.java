e73099e4b494cc817f417b0ba77d5553cbe3b7e9
Kreggy <filip.makrlik@gmail.com>
2020-12-01 10:27:47 +0100

    first

---
+++ src/main/java/cz/educanet/webik/UserResource.java
@@ -0,0 +1,10 @@
+package cz.educanet.webik;
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+
+//@Path("users") //localhost:8080/webik/api/users
+@Produces(MediaType.APPLICATION_JSON)
+public class UserResource {
+
+}
